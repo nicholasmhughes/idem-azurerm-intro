041702bab2210aa3dbb16ddd52860dd6cff40dd6
nicholasmhughes <nicholasmhughes@gmail.com>
2020-05-27 21:20:30 -0400

    updates for idem-azurerm 2.2

--- unicycle/poppy/poppy/rpc/azurerm.py
+++ unicycle/poppy/poppy/rpc/azurerm.py
@@ -1,5 +1,5 @@
 async def rg_list(hub, **kwargs):
     acct = await hub.acct.init.gather(["azurerm"], "default")
-    kwargs.update(acct or {})
-    ret = await hub.exec.azurerm.resource.group.list(**kwargs)
+    ctx = {"acct": acct}
+    ret = await hub.exec.azurerm.resource.group.list(ctx, **kwargs)
     return ret
